2eaf5a3c59c587bfaa144e94cc544dae412ccfc3
Alexander Wallin <office@alexanderwallin.com>
2017-08-01 00:30:37 +0100

    Link list creator page with fake functionality

--- src/components/LinkLists.js
+++ src/components/LinkLists.js
@@ -1,13 +1,61 @@
 import { h, Component } from 'preact'
+import { autobind } from 'core-decorators'
 
 /**
  * Link List
  */
 class LinkList extends Component {
-  render() {
+  state = {
+    query: '',
+    isLoading: false,
+    results: [],
+  }
+
+  @autobind
+  handleQueryChange(evt) {
+    console.log('handleQueryChange', { evt })
+    this.setState(
+      () => ({
+        query: evt.target.value,
+        isLoading: true,
+      }),
+      () => {
+        setTimeout(() => {
+          this.setState(() => ({
+            isLoading: false,
+            results: [Date.now()],
+          }))
+        }, 2000)
+      }
+    )
+  }
+
+  render(props, { query, isLoading, results }) {
     return (
       <div className="LinkList">
         <h1>Link List Creator</h1>
+
+        <h2>Query</h2>
+
+        <textarea
+          placeholder="Paste titles or URLs here"
+          value={query}
+          onChange={this.handleQueryChange}
+        />
+
+        <hr />
+
+        <h2>Results</h2>
+
+        {isLoading
+          ? <p>Loading...</p>
+          : <ul>
+              {results.map(result =>
+                <li key={result}>
+                  {result}
+                </li>
+              )}
+            </ul>}
       </div>
     )
   }
