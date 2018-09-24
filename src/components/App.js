f1becab40bbe752302b040496fc7f307eb4065b9
Alexander Wallin <office@alexanderwallin.com>
2018-09-24 22:59:54 +0200

    Introduces styled-components

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
 import { h, Component } from 'preact'
 import { Route } from 'react-router-dom'
+import styled from 'styled-components'
 
 import Header from 'src/components/Header.js'
 import LinkLists from 'src/components/LinkLists.js'
 
+const AppContainer = styled.div`
+  width: 90%;
+  max-width: 960px;
+  margin: 0 auto;
+`
+
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <AppContainer>
         <Header />
 
         <div>
           <Route exact path="/" render={() => <p>🎷</p>} />
           <Route exact path="/link-lists" component={LinkLists} />
         </div>
-      </div>
+      </AppContainer>
     )
   }
 }
