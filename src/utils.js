8e1b6c25a86f9a7d269fa74a5cfdf9e97bd4ea26
Alexander Wallin <office@alexanderwallin.com>
2019-05-19 17:08:27 +0200

    Addresses linting issues

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
+/* eslint import/prefer-default-export: 0 */
 import isAbsoluteUrl from 'is-absolute-url'
 
 import { QueryType } from 'src/constants.js'
@@ -5,7 +6,7 @@ import { QueryType } from 'src/constants.js'
 export const getQueryType = query => {
   if (isAbsoluteUrl(query)) {
     return QueryType.YOUTUBE_URL
-  } else {
-    return QueryType.FREE_TEXT
   }
+
+  return QueryType.FREE_TEXT
 }
